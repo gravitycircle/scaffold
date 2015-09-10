306b11f869b3db0754db3dac4aed6d404d6953ef
gravitycircle <richard.y.ong@outlook.com>
2015-09-10 15:18:11 -0700

    Create introductory remarks page.

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('CANONICAL', 'http://localhost/scaffolding/');
-define('BASE', 'http://localhost/scaffolding/');
+define('CANONICAL', 'http://localhost/scaffold/');
+define('BASE', 'http://localhost/scaffold/');
 ?>
--- index.php
+++ index.php
@@ -7,18 +7,54 @@ include_once('config.php');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Angular: Site Scaffolding & Bootstrap</title>
-	<link rel="shortcut icon" href="<?=BASE?>img/favico.ico" type="image/vnd.microsoft.icon" />
-	<link rel="icon" type="image/png" href="<?=BASE?>img/favico.png" />
-	<script type="text/javascript" src="lib/modernizr.js"></script> 
-	<script type="text/javascript" src="lib/jquery.js"></script>
-	<script type="text/javascript" src="lib/angular.js"></script>
-	<script type="text/javascript" src="lib/transit.js"></script>
-	<script type="text/javascript" src="scr/config.js.php"></script>
-	<script type="text/javascript" src="scr/views.js"></script>
-	<script type="text/javascript" src="scr/html.js"></script>
-	<script type="text/javascript" src="scr/main.js"></script>
+	<link rel="shortcut icon" href="img/favico.ico" type="image/vnd.microsoft.icon" />
+	<link rel="icon" type="image/png" href="img/favico.png" />
 	<link rel="stylesheet" href="css/style.css" />
 	<base href="<?=BASE?>">
 </head>
-<body class="isLoading contentLoading"></body>
+<body>
+	<div id="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 style="border-bottom: 1px solid #ccc; padding-bottom: 20px"><img src="img/favico.png" alt="---"> Angular: Site Scaffolding & Bootstrap v0.2</h1>
+					<p>This is the starting point of every new site you'll create using a combination of the following frameworks:</p>
+					<ul>
+						<li>
+							<strong>Javascript</strong>
+							<ul>
+								<li>AngularJS v1.3.15</li>
+								<li>jQuery v2.1.3</li>
+							</ul>
+						</li>
+						<li>
+							<strong>Preprocessors</strong>
+							<ul>
+								<li>Compass (requires Ruby and gem installation)</li>
+								<li>SASS (requires Ruby and gem installation)</li>
+								<li>PHP v5.6.11 (requires running on a server. Preferrably Apache 2.0)</li>
+							</ul>
+						</li>
+						<li>
+							<strong>Markup and Styling</strong>
+							<ul>
+								<li>HTML 5</li>
+								<li>CSS</li>
+							</ul>
+						</li>
+					</ul>
+					<p>Please make sure your config.php is set up to the right Base address by changing the BASE and CANONICAL constants. You'll know that you've set it correctly if you see a red logo on the top of this page after a refresh. Currently the constants are set to:</p>
+					<ul>
+						<li><strong>BASE: </strong><?=BASE?></li>
+						<li><strong>CANONICAL: </strong><?=CANONICAL?></li>
+					</ul>
+					<p>Also note that BASE refers to the base URL of the site itself. CANONICAL refers to the base URL of any API endpoint you will be using that runs alongside this application.</p>
+
+					<p>This is only the introduction page. The installation file for this scaffolding setup has not yet configured since the framework stack is still in active development. To start working with the stack anyway, please rename this file into something different and rename the _index.php file to index.php. </p>
+					<h4 style="border-top: 1px solid #ccc; padding-top: 20px;">~ Happy Coding!</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
 </html>
