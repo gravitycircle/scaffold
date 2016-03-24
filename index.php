43ec79c5b14abacb78b33e945993c23181fa1eb7
gravitycircle <richard.y.ong@outlook.com>
2016-03-23 17:50:59 -0700

    Merge fix

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 include_once('config.php');
-<<<<<<< HEAD
 include_once('php/server.php');
-=======
 include_once(DOCROOT.'/_data/main.php');
 include_once('php/server.php');
 $gen_data = main(false);
->>>>>>> master
+
 ob_start();
 ?>
 <!DOCTYPE html>
@@ -14,7 +12,6 @@ ob_start();
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<<<<<<< HEAD
 	<title>Angular: Site Scaffolding & Bootstrap</title>
 	<link rel="shortcut icon" href="<?=BASE?>img/favico.ico" type="image/vnd.microsoft.icon" />
 	<link rel="icon" type="image/png" href="<?=BASE?>img/favico.png" />
@@ -23,14 +20,12 @@ ob_start();
 	<script type="text/javascript" src="lib/jquery.js"></script>
 	<script type="text/javascript" src="lib/angular.js"></script>
 	<script type="text/javascript" src="lib/angular-maps.js"></script>
-=======
 	<title><?=$gen_data['site_name']?></title>
 	<link rel="shortcut icon" href="<?=BASE?>img/favico.ico" type="image/vnd.microsoft.icon" />
 	<link rel="icon" type="image/png" href="<?=BASE?>img/favico.png" />
 	<script type="text/javascript" src="lib/modernizr.js"></script> 
 	<script type="text/javascript" src="lib/jquery.js"></script>
 	<script type="text/javascript" src="lib/angular.js"></script>
->>>>>>> master
 	<script type="text/javascript" src="lib/transit.js"></script>
 	<script type="text/javascript" src="scr/config.js.php"></script>
 	<script type="text/javascript" src="scr/views.js"></script>
@@ -40,11 +35,8 @@ ob_start();
 	<link rel="stylesheet" href="css/style.css" />
 	<base href="<?=BASE?>">
 </head>
-<<<<<<< HEAD
 <body class="isLoading contentLoading"></body>
-=======
 <body></body>
->>>>>>> master
 </html>
 <?php
 echo _serve(ob_get_clean());
