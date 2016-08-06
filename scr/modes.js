7aa199b013f5d4b89dc83c4a53b28b8d661cf017
Richard Ong <richard@kevinlouisdesign.com>
2016-08-05 19:00:31 -0700

    Bookmark: Editing styles in _sass/fields-table.scss

--- scr/modes.js
+++ scr/modes.js
@@ -108,43 +108,36 @@
 		return{
 			data : {},
 			required: [],
-			set : function(name, value, filter, type){
-				var d = this;
-				var r = false;
-				if(type == 'dropdown'){
-					r = true;
-				}
-				else
-				{
-					if(filter[0] == 'require'){
+			set : function(name, value, require, type){
+				if(type != 'submit') {
+					var d = this;
+					var r = false;
+					if(require){
 						r = true;
 					}
-				}
 
-				if(r && value === ''){
-					d.data[name] = false;
-				}else{
-					d.data[name] = value;
+					if(r && value === ''){
+						d.data[name] = false;
+					}else{
+						d.data[name] = value;
+					}
 				}
 			},
-			register : function(name, filter, type){
-				var d = this;
-				var r = false;
-				if(type == 'dropdown'){
-					r = true;
-				}
-				else
-				{
-					if(filter[0] == 'require'){
+			register : function(name, require, type){
+				if(type != 'submit') {
+					var d = this;
+					var r = false;
+					
+					if(require){
 						r = true;
 					}
-				}
 
-				if(r){
-					d.data[name] = false;
-					d.required.push(name);
-				}else{
-					d.data[name] = '';
+					if(r){
+						d.data[name] = false;
+						d.required.push(name);
+					}else{
+						d.data[name] = '';
+					}
 				}
 			},
 			reset : function(){
@@ -183,8 +176,8 @@
 				});
 				var g = null;
 				$scope.initialize = function(){
-					$scope.field = $scope.$parent.regform[$attrs.side][$attrs.index];
-					submission.register($scope.field.id, $scope.field.filter, $scope.field.type);
+					$scope.field = $scope.$parent.registration.fields[$attrs.index];
+					submission.register($scope.field.id, $scope.field.require, $scope.field.type);
 					var compose;
 					if($scope.field.type == 'text' || $scope.field.type == 'email'){
 						compose = '<text-field class="rendering"></text-field>';
@@ -192,11 +185,17 @@
 					else if($scope.field.type == 'dropdown'){
 						compose = '<dropdown-field class="rendering"></dropdown-field>';
 					}
-					else if($scope.field.type == 'bool'){
-						compose = '<bool-field class="rendering"></bool-field>';
+					else if($scope.field.type == 'checkbox'){
+						compose = '<checkbox-field class="rendering"></checkbox-field>';
 					}
-					else if($scope.field.type == 'check'){
-						compose = '<check-field class="rendering"></check-field>';
+					else if($scope.field.type == 'paragraph'){
+						compose = '<paragraph-field class="rendering"></paragraph-field>';
+					}
+					else if($scope.filed.type == 'switch') {
+						compose = '<switch-field class="rendering"></switch-field>';
+					}
+					else if($scope.field.type == 'submit'){
+						compose = '<submit-field class="rendering"></submit-field>';
 					}
 
 					$($element).append(compose);
@@ -204,6 +203,72 @@
 					g = $scope.$new();
 
 					$compile($($element).find('.rendering'))(g);
+				};
+			}
+		};
+	}]);
+
+	mode.directive('switchField', ['submission', function(submission){
+		return {
+			restrict: 'E',
+			templateUrl: 'shadow/fields/switch.html',
+			replace: true,
+			scope : {
+				render: '@',
+				mrk: '@'
+			},
+			controller: function($scope, $element, $attrs){
+				$scope.render = $scope.$parent.field;
+
+				if($scope.$parent.field.filter[0] == 'require'){
+					$scope.mrk = '*';
+				}
+				else{
+					$scope.mrk = '';
+				}
+
+				
+			}
+		};
+	}]);
+
+	mode.directive('checkboxField', ['submission', function(submission){
+		return{
+			restrict: 'E',
+			templateUrl: 'shadow/fields/checkbox.html',
+			replace: true,
+			scope: {
+				render: '@',
+				mrk: '@'
+			},
+			link: function(scope, element, attrs) {
+
+			},
+			controller: function($scope, $element, $attrs) {
+				$scope.render = $scope.$parent.field;
+				$scope.render.idname = $scope.render.id.replace('[', '-').replace(']', '');
+				if($scope.$parent.field.require){
+					$scope.mrk = '*';
+				}
+				else{
+					$scope.mrk = '';
+				}
+
+				$scope.toggle = function(){
+					$('.field-handler').removeClass('red');
+					if(!$($element).find('label').hasClass('check')){
+						$($element).find('label').addClass('check');
+						submission.set($scope.render.id, $scope.$parent.field.value, $scope.render.require, $scope.render.type);
+					}
+					else{
+						$($element).find('label').removeClass('check');
+						if($scope.$parent.require) {
+							submission.set($scope.render.id, false, $scope.render.require, $scope.render.type);
+						}
+						else {
+							submission.set($scope.render.id, '', $scope.render.require, $scope.render.type);
+						}
+					}
 				};
 			}
 		};
@@ -223,8 +288,8 @@
 			},
 			controller: function($scope, $element, $attrs){
 				$scope.render = $scope.$parent.field;
-
-				if($scope.$parent.field.filter[0] == 'require'){
+				$scope.render.idname = $scope.render.id.replace('[', '-').replace(']', '');
+				if($scope.$parent.field.require){
 					$scope.mrk = '*';
 				}
 				else{
@@ -232,7 +297,8 @@
 				}
 
 				$scope.typing = function(){
-					submission.set($scope.render.id, $($element).find('input').val(), $scope.render.filter, $scope.render.type);
+					$('.field-handler').removeClass('red');
+					submission.set($scope.render.id, $($element).find('input').val(), $scope.render.require, $scope.render.type);
 					if($($element).find('.input-error').length > 0){
 						$($element).find('.input-error').removeClass('input-error');
 					}
@@ -245,90 +311,51 @@
 		};
 	}]);
 
-	mode.directive('checkField', ['submission', function(submission){
+	mode.directive('paragraphField', ['submission', function(submission){
 		return{
 			restrict: 'E',
-			templateUrl: 'shadow/fields/check.html',
+			templateUrl: 'shadow/fields/paragraph.html',
 			scope: {
 				render: '@',
 				mrk: '@'
 			},
 			replace: true,
-			link: function(scope, element, attrs){
-
-			},
-			controller: function($scope, $element, $attrs){
-				var checked = false;
+			controller: function($scope, $element, $attrs) {
 				$scope.render = $scope.$parent.field;
-				if($scope.$parent.field.filter[0] == 'require'){
+				$scope.render.idname = $scope.render.id.replace('[', '-').replace(']', '');
+
+				if($scope.$parent.field.require){
 					$scope.mrk = '*';
 				}
 				else{
 					$scope.mrk = '';
 				}
 
-				$scope.toggle = function(){
-					if(checked){
-						submission.set($scope.render.id, false, $scope.render.filter, $scope.render.type);
-						checked = false;
-						$($element).find('.ticker').removeClass('icon-checked');
-						$($element).find('.ticker').addClass('icon-unchecked');
+				$scope.typing = function(){
+					$('.field-handler').removeClass('red');
+					submission.set($scope.render.id, $($element).find('input').val(), $scope.render.require, $scope.render.type);
+
+					$($element).find('textarea').scrollTop(0);
+
+					if($($element).find('.input-error').length > 0){
+						$($element).find('.input-error').removeClass('input-error');
+					}
+
+					if($('body').find('.input-error').length < 1){
+						$('body').find('.error-msg').removeClass('shown');
+					}
+				};
+
+				$scope.focus = function(isFocused) {
+					if(typeof isFocused != 'boolean') {
+						isFocus = false;
+					}
+
+					if(!isFocused) {
+						$($element).find('.paragraph-sizer').removeClass('focus');
 					}
 					else{
-						submission.set($scope.render.id, 'YES', $scope.render.filter, $scope.render.type);
-						checked = true;
-						$($element).find('.ticker').addClass('icon-checked');
-						$($element).find('.ticker').removeClass('icon-unchecked');
-					}
-
-					if($($element).find('.input-error').length > 0){
-						$($element).find('.input-error').removeClass('input-error');
-					}
-
-					if($('body').find('.input-error').length < 1){
-						$('body').find('.error-msg').removeClass('shown');
-					}
-				};
-			}
-		};
-	}]);
-
-	mode.directive('boolField', ['submission', function(submission){
-		return{
-			restrict: 'E',
-			templateUrl: 'shadow/fields/bool.html',
-			scope: {
-				render: '@',
-				mrk: '@'
-			},
-			replace: true,
-			link: function(scope, element, attrs){
-
-			},
-			controller: function($scope, $element, $attrs){
-				var checked = false;
-				$scope.render = $scope.$parent.field;
-				if($scope.$parent.field.filter[0] == 'require'){
-					$scope.mrk = '*';
-				}
-				else{
-					$scope.mrk = '';
-				}
-
-				$scope.toggle = function(yn){
-					submission.set($scope.render.id, yn.toUpperCase(), $scope.render.filter, $scope.render.type);
-					$($element).find('.ticker').addClass('icon-unchecked');
-					$($element).find('.ticker').removeClass('icon-checked');
-
-					$($element).find('.answer-'+yn).removeClass('icon-unchecked');
-					$($element).find('.answer-'+yn).addClass('icon-checked');
-
-					if($($element).find('.input-error').length > 0){
-						$($element).find('.input-error').removeClass('input-error');
-					}
-
-					if($('body').find('.input-error').length < 1){
-						$('body').find('.error-msg').removeClass('shown');
+						$($element).find('.paragraph-sizer').addClass('focus');
 					}
 				};
 			}
@@ -348,25 +375,48 @@
 			},
 			controller: function($scope, $element, $attrs){
 				$scope.render = $scope.$parent.field;
+				$scope.render.idname = $scope.render.id.replace('[', '-').replace(']', '');
+				if($scope.$parent.field.require){
+					$scope.mrk = '*';
+				}
+				else{
+					$scope.mrk = '';
+				}
 				var open;
 				$scope.toggle = function(){
+
 					$('.options-container').css({
 						'height' : 0
 					});
 
-					if($($element).find('.options-container').height() > 1){
+					$('.cell').removeClass('opened');
+
+					if($($element).find('.options-container').height() > 2){
 						open = true;
 					}
 					else{
 						open = false;
 					}
 
-					var multiplier = $('.options').height();
+
+					var multiplier = $($element).find('.options').height();
 					if(!open){
+						var newheight = 0;
+						var opts = {};
+
+						if($($element).find('.options').length > 5) {
+							opts = {
+								'height' : (5 * multiplier)+'px'
+							};
+						}
+						else{
+							opts = {
+								'height' : (($($element).find('.options').length * multiplier) + 2)+'px'
+							};
+						}
+
 						$($element).find('.cell').addClass('opened');
-						$($element).find('.options-container').css({
-							'height' : ($($element).find('.options').length * multiplier)
-						});
+						$($element).find('.options-container').css(opts);
 						
 					}
 					else{
@@ -377,9 +427,10 @@
 					}
 				};
 
-				$scope.choose = function(choice){
-					$($element).find('.selected').html(choice);
-					submission.set($scope.render.id, encodeURIComponent(choice), $scope.render.filter, $scope.render.type);
+				$scope.choose = function(choice, html){
+					$('.field-handler').removeClass('red');
+					$($element).find('.selected').html(html);
+					submission.set($scope.render.id, encodeURIComponent(choice), $scope.render.require, $scope.render.type);
 
 					if($($element).find('.input-error').length > 0){
 						$($element).find('.input-error').removeClass('input-error');
@@ -388,6 +439,26 @@
 					if($('body').find('.input-error').length < 1){
 						$('body').find('.error-msg').removeClass('shown');
 					}
+				};
+			}
+		};
+	}]);
+	
+	mode.directive('submitField', ['submission', function(submission){
+		return{
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'shadow/fields/submit.html',
+			scope: {
+				render: '@'
+			},
+			controller: function($scope, $element, $attrs){
+				//do submission here
+				$scope.render = $scope.$parent.field;
+
+
+				$scope.exec = function(){
+					console.log(submission.data);
 				};
 			}
 		};
