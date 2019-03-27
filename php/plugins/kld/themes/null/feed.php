57ba70fbc1b9b33dd31c32410ff319259990a0e3
Richard <richard@iMacR.local>
2019-03-27 12:13:50 -0700

    KLD - re-added themes
    '

---
+++ php/plugins/kld/themes/null/feed.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Template Name: Feed Template
+ * Description: Page template for Post Feeds.
+ */
+header("HTTP/1.0 404 Not Found");
+wp_die('Theming completely disabled. Content on this website is strictly forbidden without API access via its companion single page application.');
+?>
---
+++ php/plugins/kld/themes/null/functions.php
@@ -0,0 +1,4 @@
+<?php
+// update_option('siteurl', 'https://local.kevinlouisdesign.com/marathon/_bin' );
+// update_option('home', 'https://local.kevinlouisdesign.com/marathon/_bin' );
+?>
